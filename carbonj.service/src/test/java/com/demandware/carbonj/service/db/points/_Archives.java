6ce288465a883b9b93939fd3b6de097c2e16a926
anadagoudar <anadagoudar@demandware.com>
2020-10-13 10:19:17 -0400

    fixing the build

--- carbonj.service/src/test/java/com/demandware/carbonj/service/db/points/_Archives.java
+++ carbonj.service/src/test/java/com/demandware/carbonj/service/db/points/_Archives.java
@@ -53,7 +53,7 @@ public class _Archives extends BaseTest
         index.open();
         StagingFiles sFiles = new StagingFiles(metricRegistry, new File( dbDirFile, "staging" ), new SystemSort(), index );
         DataPointStagingStore stagingStore = new DataPointStagingStore( metricRegistry, sFiles, 1000, 1, 1, 1, 100, 30, 3);
-        DataPointArchiveFactory pointArchiveFactory = new DataPointArchiveFactory( metricRegistry, dbDirFile, new RocksDBConfig() );
+        DataPointArchiveFactory pointArchiveFactory = new DataPointArchiveFactory( metricRegistry, dbDirFile, new RocksDBConfig(), false );
         QueryCachePolicy qcp = new QueryCachePolicy( true, true, true );
         archives = new DataPointStoreImpl( metricRegistry, pointArchiveFactory, new DatabaseMetrics(metricRegistry), stagingStore, true, 100, 10, qcp, name -> true );
         archives.open();
